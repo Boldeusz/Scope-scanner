9e0bd04e4c492c550e9375b6f91d6e9a094642a2
Boldeusz <mnpower@poczta.onet.pl>
2019-11-06 16:43:54 +0100

    Signed-off-by: Boldeusz <mnpower@poczta.onet.pl>

---
+++ src/com/example/test/Main.java
@@ -0,0 +1,15 @@
+package com.example.test;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+
+        X x = new X(new Scanner(System.in));
+        x.x();
+        //System.out.println("Your number is: " + x);
+    }
+}
